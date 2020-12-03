db42ce92c65ff7df0f47383448b465d4993f2707
Łukasz Bezłada <bezi@o2.pl>
2020-12-03 23:14:17 +0100

    Add login page

--- src/main/java/com/lukaszbezlada/studentdiary/security/SecurityConfig.java
+++ src/main/java/com/lukaszbezlada/studentdiary/security/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionCreationPolicy(SessionCreationPolicy.ALWAYS);
         http
                 .authorizeRequests()
-                .antMatchers("/static/**", "/css/**", "/img/**", "/js/**", "/scss/**", "/vendor/**").permitAll()
+                .antMatchers("/static/**", "/css/**", "/img/**", "/js/**").permitAll()
                 .antMatchers("/").permitAll()
                 .anyRequest().authenticated()
                 .and()
