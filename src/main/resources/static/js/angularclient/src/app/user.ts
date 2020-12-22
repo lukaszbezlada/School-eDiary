852cf4d93048ad28062c4422e88c390eb164df7f
Łukasz Bezłada <bezi@o2.pl>
2020-12-22 16:33:03 +0100

    Configuring Angular app

---
+++ src/main/resources/static/js/angularclient/src/app/user.ts
@@ -0,0 +1,19 @@
+export class User {
+
+  id: string;
+  login: string;
+  password: string;
+  password2: string;
+}
+
+// String
+// firstName;
+//
+// String
+// lastName;
+//
+// String
+// email;
+//
+// Set < UserRole > roles = new HashSet<>();
+
