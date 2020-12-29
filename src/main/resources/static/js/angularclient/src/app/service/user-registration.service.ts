377a62afcf804a83440309c252017dfd61919da5
Łukasz Bezłada <bezi@o2.pl>
2020-12-29 16:25:40 +0100

    registration component

---
+++ src/main/resources/static/js/angularclient/src/app/service/user-registration.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {User} from "../user";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserRegistrationService {
+
+  constructor(private http:HttpClient) { }
+
+  public doRegistration(user: User) {
+    return this.http.post("http://localhost:8080/api/users/addUser", user, {responseType:"text" as 'json'});
+  }
+}
